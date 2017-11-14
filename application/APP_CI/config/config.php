f59bdc366c7119a01a9688d69aa30e6fdd8af838
Fahmi Alfath <fahmi243alfath@gmail.com>
2017-11-14 08:52:17 +0700

    ignored

--- application/APP_CI/config/config.php
+++ application/APP_CI/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . '/transaksi';
+$config['base_url'] = 'http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . '/client/almindo_transaksi';
 
 /*
 |--------------------------------------------------------------------------
