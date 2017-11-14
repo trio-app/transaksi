0a12f14ce9cab310be9b80d3d529b7c864e95b9a
fedyhendrawan3 <33412826+fedyhendrawan3@users.noreply.github.com>
2017-11-14 09:31:36 +0700

    Master Warnaglasin finish

---
+++ application/APP/Mwarnaglasin/controller/C_mwarnaglasin.js
@@ -0,0 +1,118 @@
+		Ext.define('Almindo.Mwarnaglasin.controller.C_mwarnaglasin',{
+			extend: 'Ext.app.Controller',
+			views: [
+				'Almindo.Mwarnaglasin.view.GRID_mwarnaglasin',
+				'Almindo.Mwarnaglasin.view.FRM_mwarnaglasin'
+			],
+			stores  : [
+				//'Almindo.Mwarnaglasin.store.ST_mwarnaglasin'
+			],
+			refs: [{
+				ref: 'FRM_mwarnaglasin',
+				xtype: 'FRM_mwarnaglasin',
+				selector: 'FRM_mwarnaglasin',
+				autoCreate: true
+			},{
+				ref: 'GRID_mwarnaglasin',
+				xtype: 'GRID_mwarnaglasin',
+				selector: 'GRID_mwarnaglasin',
+				autoCreate: true
+			}],
+			init: function(){
+				this.control({
+					'GRID_mwarnaglasin > toolbar > textfield[itemId=searchData]': {
+						specialkey: this.searchData
+					},										
+					'GRID_mwarnaglasin' :{
+						itemdblclick: this.onRowdblclick,
+						removeitem: this.deleteItem
+					},
+					'FRM_mwarnaglasin button[action=add]':{
+						click: this.doSaveform
+					},
+				});
+			},
+			searchData:function (f,e) {
+				var store = Ext.getStore('Almindo.Mwarnaglasin.store.ST_mwarnaglasin');//Ext.getStore('Almindo.Mwarnaglasin.store.ST_mwarnaglasin');//
+				if (e.getKey() == e.ENTER) {
+					store.remoteFilter = false;
+					store.clearFilter();
+					store.remoteFilter = true;
+					store.filter([{
+						property:'filtername',
+						anyMatch: true,
+						value   : f.value
+					} ]);
+				}
+
+			},
+			onRowdblclick: function(me, record, item, index){							
+				var win = this.getFRM_mwarnaglasin();
+				win.setAction('edit');
+				win.setRecordIndex(index);
+				win.down('form').getForm().setValues(record.getData());
+							
+				win.show();
+			},
+			deleteItem:function (record) {
+				Ext.Msg.confirm('Delete Warna Glasin', 'Are you sure?', function (button) {
+					if (button == 'yes') {
+						this.doProsesCRUD('delete',record);
+					}
+				}, this);
+			},
+			doProsesCRUD : function (inAction,record){
+				var win = this.getFRM_mwarnaglasin();
+				var grid = this.getGRID_mwarnaglasin();
+				var store = grid.getStore();//Ext.getStore('ScontactStore');
+				Ext.Ajax.request({
+					url: base_url + 'Mwarnaglasin/' +  inAction,
+					method: 'POST',
+					type:'json',
+					params: JSON.stringify(record.data),
+					success: function(response){
+						switch(inAction) {
+							case 'delete':
+									store.load();
+									createAlert('Delete Warna Glasin', 'Delete Data Success', 'success');
+								break;
+							case 'create' :
+									store.load();
+									createAlert('Insert Warna Glasin', 'Insert Data Success', 'success');
+								break;
+							case 'update' :
+									store.load();
+									createAlert('Update Warna Glasin', 'Update Data Success', 'success');
+								break;
+						}
+                            win.down('form').getForm().reset();
+                            win.setAction('add');
+
+					},
+					failure: function(response){
+						Ext.Msg.alert('server-side failure with status code ' + response.status  , response.responseText);
+
+					}
+				});
+			},						
+			doSaveform: function(){
+				var win = this.getFRM_mwarnaglasin();
+				var store = Ext.getStore('Almindo.Mwarnaglasin.store.ST_mwarnaglasin');
+				var form = win.down('form');
+				var values = form.getValues();
+				var record = form.getRecord();
+				var action = win.getAction();
+				var recValue = Ext.create('Almindo.Mwarnaglasin.model.M_mwarnaglasin', values);
+				console.log(action);
+								
+				if(action == 'edit'){
+					if(form.isValid()){
+						this.doProsesCRUD('update',recValue);
+					}
+				}else{
+					if(form.isValid()){
+						this.doProsesCRUD('create',recValue);
+					}
+				}
+			}			
+		});
---
+++ application/APP/Mwarnaglasin/view/FRM_mwarnaglasin.js
@@ -0,0 +1,51 @@
+Ext.define('Almindo.Mwarnaglasin.view.FRM_mwarnaglasin',{
+    extend: 'Ext.form.Panel',
+    alias: 'widget.FRM_mwarnaglasin',
+    frame: false,
+    border: 0,
+    config: {
+            recordIndex: 0,
+            action: ''
+    },   
+   items: [{
+        xtype: 'box',
+        html: '<p style="font-size: 14pt;">Form Master Warna Glasin</p>',
+        },{
+        xtype: 'form',
+        border: 0,
+        layout: 'anchor',
+        defaults: {
+            xtype: 'textfield',
+            anchor: '100%'
+        },
+        items: [{
+            readOnly: true,
+            xtype: 'hidden',
+            name: 'warnaglasin_id',
+            fieldLabel: 'ID'
+        },{
+            name: 'warnaglasin_nama',
+            flex: 1,
+            fieldLabel: 'Nama Warna Glasin ',
+            labelAlign: 'top',
+            allowBlank: false
+        },{
+            name: 'warnaglasin_desc',
+            flex: 1,
+            fieldLabel: 'Description ',
+            labelAlign: 'top',
+            //allowBlank: false
+        }]
+}],
+    buttons: [{
+        text: 'Save',
+        action: 'add'
+    },{
+        text    : 'Reset',
+        handler : function () { 
+            var frm = this.up('panel');
+            frm.down('form').getForm().reset(); 
+            frm.setAction('add');
+        }
+    }] 
+});
