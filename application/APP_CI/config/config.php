a1267b5ba354b6542eb75462847f08d48f885f99
Fahmi Alfath <fahmi243alfath@gmail.com>
2017-11-14 13:55:27 +0700

    s

--- application/APP_CI/config/config.php
+++ application/APP_CI/config/config.php
@@ -23,15 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 $config['base_url'] = 'http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . '/client/almindo_transaksi';
-=======
-$config['base_url'] = 'http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . '/Github/transaksi';
->>>>>>> 90d0a1e2e73b7070f2073c072ac5a0d97fb06f69
-=======
-$config['base_url'] = 'http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . '/transaksi';
->>>>>>> a2eb21e183f6cdab305cf10cc830637817f53b6f
 
 /*
 |--------------------------------------------------------------------------
